38c4e99ad3321ed01a70378c976ad950278b00e3
Erik Landaverde <landaverde.erik10@gmail.com>
2023-04-02 09:57:54 -0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var corsSpecs = "CorsSpecs";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsSpecs,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000",
+                                              "https://www.mely.photos/",
+                                              "http://www.mely.photos/");
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseCors(corsSpecs);
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
