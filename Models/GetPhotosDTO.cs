59fbc76bb72d2cf6377417a3976728e93af1846d
Erik Landaverde <landaverde.erik10@gmail.com>
2023-04-02 09:59:30 -0700

    commit 2

---
+++ Models/GetPhotosDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MelyPhotography.Models
+{
+    public class GetPhotosDTO
+    {
+        public List<string> photos { get; set; } = null;
+        public bool success { get; set; }
+    }
+}
---
+++ Models/UploadDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MelyPhotography.Models
+{
+    public class UploadDTO
+    {
+        public List<string> files { get; set; } = null;
+        public bool success { get; set; }
+    }
+}
